4336e84deaf7b2472b4abe9d2fcad135c4c1fbcb
csuilong <csuilong@163.com>
2020-02-26 03:09:56 +0800

    数据库组件

--- config/database.go
+++ config/database.go
@@ -1,17 +1,20 @@
 package Config
 
 import (
+	"fmt"
 	"gin-laravel/app/Models"
 	"github.com/jinzhu/gorm"
 )
 
 func InitDatabase() {
 	var err error
-	Db, err = gorm.Open("sqlite3", "test.db?cache=shared")
+	Db, err = gorm.Open("sqlite3", App.GetString("sqlite.url"))
 	if err != nil {
 		panic("连接数据库失败" + err.Error())
 	}
 
 	Db.AutoMigrate(&Models.UserModel{})
-
+	fmt.Printf("sqlite3数据库连接成功: %s \r\n",
+		App.GetString("sqlite.url"),
+	)
 }
